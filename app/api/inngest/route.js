0886c92dbadfa41ebe0a3f01add85dbc9c6ab326
devdeepak <deepakptdr2209@gmail.com>
2025-03-22 14:41:23 +0530

    (Feat) : written cron job in inngest to fetch the insight every week

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,7 @@
 import { inngest } from "@/lib/inngest/client";
+import { generateIndustryInsight } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
-
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    /* your functions will be passed here later! */
-  ],
+  functions: [generateIndustryInsight],
 });
